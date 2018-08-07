2fc23a7655b9d908dc3f578aba9cb2a537c4c59d
Jacob Berman <jacobberman1995@gmail.com>
2018-08-07 02:12:16 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -18,13 +18,16 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
-
+def pizza_party (arguement = "cheese")
+  return arguement
+  
 end
+
